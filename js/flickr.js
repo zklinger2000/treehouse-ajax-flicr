76132d390ca8e75c1843efe64715f4f7c3a36d2e
Zack Klinger <zklinger@gmail.com>
2015-05-31 16:25:00 -0700

    ADD callback function logic

--- js/flickr.js
+++ js/flickr.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     
     //VARIABLES for using the flickr.com API
     //the url to public photo feed
-    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne/jsoncallback=?";
+    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     //the text from the button that was clicked
     var animal = $(this).text();
     //jQuery expects a JavaScript object for options below
@@ -19,8 +19,20 @@ $(document).ready(function () {
     };
     //this is our callback function
     function displayPhotos(data) {
-      
-    }
+      var photoHTML = '<ul>';
+      $.each(data.items, function (i, photo) {
+        //adding the class for the CSS
+        photoHTML += '<li class="grid-25 tablet-grid-50">';
+        //adding the link to the image
+        photoHTML += '<a href="' + photo.link + '" class="image">';
+        //adding the <image>
+        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+      });//END each
+      //add closing list tag
+      photoHTML += '</ul>';
+      //use jQuery to replace the html in the div id='photos'
+      $('#photos').html(photoHTML);
+    }//END callback function displayPhotos(data)
     //this is sending an AJAX request with url, JavaScript object, and callback function
     $.getJSON(flickrAPI, flickrOptions, displayPhotos);
   });
