62ce3b4e1a4cd245ea6310f9b5420dd3a8195199
Zack Klinger <zklinger@gmail.com>
2015-05-31 16:04:37 -0700

    ADD the url, JavaScript object used for options, and empty callback function

--- js/flickr.js
+++ js/flickr.js
@@ -6,5 +6,38 @@ $(document).ready(function () {
     $('button').removeClass('selected');
     //add a class to the clicked on button
     $(this).addClass('selected');
+    
+    //VARIABLES for using the flickr.com API
+    //the url to public photo feed
+    var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne/jsoncallback=?";
+    //the text from the button that was clicked
+    var animal = $(this).text();
+    //jQuery expects a JavaScript object for options below
+    var flickrOptions = {
+      tags: animal,
+      format: "json"
+    };
+    //this is our callback function
+    function displayPhotos(data) {
+      
+    }
+    //this is sending an AJAX request with url, JavaScript object, and callback function
+    $.getJSON(flickrAPI, flickrOptions, displayPhotos);
   });
 });//END ready
+
+/*
+THis is an alternative way to do the getJSON call that we will see
+getJSON("http://api.flickr.com/services/feeds/photos_public.gne/jsoncallback=?",
+        {
+          tags: animal,
+          format: "json"
+        },
+        function (data) {
+        
+        }
+       );//END getJSON
+
+
+
+*/
